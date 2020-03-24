b7b250d364b3e81e8fba8e5c89da0c3c04859240
Javante Nelson <javantenelson@gmail.com>
2020-03-24 23:02:58 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,6 @@
 def square_array(array)
  new_product=[]
- array.each do |product| new_product <<product**2
-                
-          end
+ array.each do |product| new_product << product**2
+            end
 new_product
 end
